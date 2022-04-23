4b9195ef52996f457794be9c7d06fa3351da604a
Andrew Owino <andystudy4@gmail.com>
2022-04-23 15:47:23 +0300

    Configure and test fa icons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app" class="container">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius hic iste quae sapiente itaque illum quia quam molestias accusamus eum veniam quisquam nihil dicta sunt reiciendis, architecto molestiae. Inventore, odio.</p>
+    <p> <font-awesome-icon icon="trash"/> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius hic iste quae sapiente itaque illum quia quam molestias accusamus eum veniam quisquam nihil dicta sunt reiciendis, architecto molestiae. Inventore, odio.</p>
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    FontAwesomeIcon
+  }
 }
 </script>
 
